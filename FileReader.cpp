9ce02fd09c461d4ea3e032b4f9fd9d1ab0f1c030
KristinaRiabova <kristinaer304@gmail.com>
2023-10-25 11:38:13 +0300

    Chunk Mode for File Reader/Writer

--- FileReader.cpp
+++ FileReader.cpp
@@ -3,17 +3,21 @@
 #include <stdexcept>
 
 std::string FileReader::read(const std::string& filePath) {
-    std::ifstream file(filePath);
+    std::ifstream file(filePath, std::ios::binary);
     if (!file.is_open()) {
         throw std::runtime_error("File not found or could not be opened.");
     }
 
     std::string content;
-    char buffer[128];
-    while (!file.eof()) {
-        file.read(buffer, sizeof(buffer));
-        content.append(buffer, file.gcount());
+    const std::size_t chunkSize = 128;
+    char buffer[chunkSize];
+
+    while (file.read(buffer, chunkSize)) {
+        content.append(buffer, chunkSize);
     }
+
+
+    content.append(buffer, file.gcount());
 
     file.close();
     return content;
--- FileWriter.cpp
+++ FileWriter.cpp
@@ -3,13 +3,18 @@
 #include <stdexcept>
 
 void FileWriter::write(const std::string& filePath, const std::string& content) {
-    std::ifstream file(filePath);
-    if (file.good()) {
-        throw std::runtime_error("File already exists. Cannot overwrite.");
+    std::ofstream outFile(filePath, std::ios::binary);
+    if (!outFile.is_open()) {
+        throw std::runtime_error("Failed to open file for writing.");
     }
 
-    std::ofstream outFile(filePath);
-    outFile << content;
+    const std::size_t chunkSize = 128;
+
+    for (std::size_t pos = 0; pos < content.length(); pos += chunkSize) {
+        std::string chunk = content.substr(pos, chunkSize);
+        outFile.write(chunk.c_str(), chunk.size());
+    }
+
     outFile.close();
 }
 
